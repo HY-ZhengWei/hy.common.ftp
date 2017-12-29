43214dd09e85a9ea6945086db99d240c09be452c
HY-ZhengWei <hy.zhengwei@qq.com>
2017-12-29 16:23:12 +0800

    1.0.1
    Signed-off-by: HY-ZhengWei <hy.zhengwei@qq.com>

--- Common.003.Win.FTP.bat
+++ Common.003.Win.FTP.bat
@@ -4,7 +4,7 @@ cd .\bin
 
 rd /s/q .\org\hy\common\ftp\junit
 
-jar cvfm hy.common.ftp.jar MANIFEST.MF LICENSE org
+jar cvfm hy.common.ftp.jar MANIFEST.MF META-INF org
 
 copy hy.common.ftp.jar ..
 del /q hy.common.ftp.jar
--- Common.004.Mac.FTP.sh
+++ Common.004.Mac.FTP.sh
@@ -4,7 +4,7 @@ cd ./bin
 
 rm -R ./org/hy/common/ftp/junit
 
-jar cvfm hy.common.ftp.jar MANIFEST.MF LICENSE org
+jar cvfm hy.common.ftp.jar MANIFEST.MF META-INF org
 
 cp hy.common.ftp.jar ..
 rm hy.common.ftp.jar
