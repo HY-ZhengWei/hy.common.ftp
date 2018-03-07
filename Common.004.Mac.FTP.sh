3d604d07b84fe3697791d6b8e12655016164c82b
HY-ZhengWei <hy.zhengwei@qq.com>
2018-03-07 12:05:24 +0800

    1.0.1

--- Common.004.Mac.FTP.sh
+++ Common.004.Mac.FTP.sh
@@ -2,7 +2,9 @@
 
 cd ./bin
 
+
 rm -R ./org/hy/common/ftp/junit
+
 
 jar cvfm hy.common.ftp.jar MANIFEST.MF META-INF org
 
@@ -10,3 +12,12 @@ cp hy.common.ftp.jar ..
 rm hy.common.ftp.jar
 cd ..
 
+
+
+
+
+cd ./src
+jar cvfm hy.common.ftp-sources.jar MANIFEST.MF META-INF org 
+cp hy.common.ftp-sources.jar ..
+rm hy.common.ftp-sources.jar
+cd ..
